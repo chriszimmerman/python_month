cdc384f01d3f489c1d39f39a44910751de8e9568
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-05 17:33:09 -0400

    getting into regex and string manipulation in python. Made a little more progress on the sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,18 @@
+import pprint
+
 print('I can solve a sudoku grid of N * N. What is N?')
 gridLength = int(input())
 
 numbers = range(1, gridLength) 
+
+grid = []
+for i in range(0, gridLength):
+    grid.append([])
+    for j in range(0, gridLength):
+        grid[i].append(j + 1)
+
+
+for i in range(0, gridLength):
+    for j in range(0, gridLength):
+       print(str(grid[i][j]), end=" ") 
+    print('')
