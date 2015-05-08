53d779a551ad70799c682ee2aa77dcf0fd35a675
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-07 21:52:31 -0400

    worked on reading in a file and getting the sudoku puzzle size dimensions

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,29 @@
 import pprint
+import os
 
-print('I can solve a sudoku grid of N * N. What is N?')
+print('I can solve a sudoku grid of N^2 * N^2. What is N?')
 gridLength = int(input())
 
 numbers = range(1, gridLength) 
 
-grid = []
-for i in range(0, gridLength):
-    grid.append([])
-    for j in range(0, gridLength):
-        grid[i].append(j + 1)
+print('What is the name of the file? It must be in the same directory as this script.')
+filename = input()
+filepath = os.getcwd() + '/' + filename
 
+if os.path.exists(filepath):
+    file = open(filepath)
+    lines = file.readlines()
+    lines = list(map(lambda line: line.strip(), lines))
+    lines = list(map(lambda line: line.split(' '), lines))
 
-for i in range(0, gridLength):
-    for j in range(0, gridLength):
-       print(str(grid[i][j]), end=" ") 
-    print('')
+    for line in lines:
+        for i in range(len(line)):
+            line[i] = int(line[i])
+    file.close()
+
+    grid = lines
+else:
+    print('Invalid filename.')
+    sys.exit(0)
+
+print(grid)
