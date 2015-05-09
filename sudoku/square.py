b8836e6bb0fe2f292cb667b275eb6db21349ee95
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-08 21:45:29 -0400

    moved sudoku code into classes

---
+++ sudoku/square.py
@@ -0,0 +1,6 @@
+class Square:
+    def __init__(self, number, row, column, block):
+        self.number = number
+        self.row = row 
+        self.column = column 
+        self.block = block
---
+++ sudoku/sudoku_file_reader.py
@@ -0,0 +1,30 @@
+import pprint
+import os
+from square import Square
+
+class SudokuFileReader:
+
+    def read_from_file(self, filename):
+        filepath = os.getcwd() + '/' + filename
+
+        try:
+            file = open(filepath)
+            lines = file.readlines()
+            file.close()
+
+            return lines
+        except IOError:
+            print("The file could not be found or is not in the right format")
+
+    def convert_file_lines_to_puzzle(self, lines):
+        lines = list(map(lambda line: line.strip(), lines))
+        lines = list(map(lambda line: line.split(' '), lines))
+
+        squares = []
+        for line in lines:
+            for i in range(len(line)):
+                number = int(line[i])
+                line[i] = Square(number, lines.index(line) + 1, i + 1, i + 1)
+            squares.append(line)
+
+        return squares
