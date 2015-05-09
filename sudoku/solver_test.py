ddeb931274aafdb0c308550b1bec0b56668dd250
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-09 08:30:02 -0400

    changed existing test to use new Square objects. Created a script that will run all of my tests.

--- sudoku/solver_test.py
+++ sudoku/solver_test.py
@@ -1,11 +1,37 @@
 import unittest
 from solver import Solver
+from square import Square
 Solver()
 
 class SudokuPuzzleSolverTest(unittest.TestCase):
 
     def test_is_valid(self):
-        puzzle = [[1,4,3,2], [3,2,4,1], [4,1,2,3], [2,3,1,4]]
+        puzzle = [
+                    [
+                        Square(1,1,1,1),
+                        Square(4,1,2,1),
+                        Square(3,1,3,2),
+                        Square(2,1,4,2)
+                    ],
+                    [
+                        Square(3,2,1,1),
+                        Square(2,2,2,1),
+                        Square(4,2,3,2),
+                        Square(1,2,4,2)
+                    ],
+                    [
+                        Square(4,2,1,3),
+                        Square(1,2,2,3),
+                        Square(2,2,3,4),
+                        Square(3,2,4,4)
+                    ],
+                    [
+                        Square(2,2,1,3),
+                        Square(3,2,2,3),
+                        Square(1,2,3,4),
+                        Square(4,2,4,4)
+                    ]
+                ]
         solver = Solver()
         self.assertTrue(solver.is_valid(puzzle))
 
