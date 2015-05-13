2069021574a40994e93c4e929fde5107f4930c23
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-12 20:40:45 -0400

    FileReader now handles puzzles with squares that do not have a number value.

--- sudoku/file_reader.py
+++ sudoku/file_reader.py
@@ -18,15 +18,16 @@ class FileReader:
             print("The file could not be found or is not in the right format.")
 
     def convert_file_lines_to_puzzle(self, lines):
+        lines = list(map(lambda line: line.strip(), lines))
         lines = list(map(lambda line: line.split(' '), lines))
 
-        puzzle= []
+        puzzle = []
         puzzleLength = len(lines)
 
         for row in range(puzzleLength):
             currentRow = []
             for column in range(puzzleLength):
-                number = int(lines[row][column])
+                number = int(lines[row][column]) if lines[row][column].isdigit() else None
                 new_square = Square(number, row + 1, column + 1, self.calculate_block(row + 1, column + 1, puzzleLength))
                 currentRow.append(new_square)
             puzzle.append(currentRow)
--- sudoku/test/file_reader_test.py
+++ sudoku/test/file_reader_test.py
@@ -48,5 +48,50 @@ class FileReaderTest(unittest.TestCase):
                 self.assertEqual(expected[i][j].column, actual[i][j].column)
                 self.assertEqual(expected[i][j].block, actual[i][j].block)
 
+    def test_convert_file_lines_to_puzzle_with_blank_squares(self):
+        lines_of_file = [
+                            '_ 2 3 _\n',
+                            '2 _ 4 1\n',
+                            '3 4 _ 2\n',
+                            '4 1 _ _\n'
+                        ]
+        expected = [
+                    [
+                        Square(None,1,1,1),
+                        Square(2,1,2,1),
+                        Square(3,1,3,2),
+                        Square(None,1,4,2)
+                    ],
+                    [
+                        Square(2,2,1,1),
+                        Square(None,2,2,1),
+                        Square(4,2,3,2),
+                        Square(1,2,4,2)
+                    ],
+                    [
+                        Square(3,3,1,3),
+                        Square(4,3,2,3),
+                        Square(None,3,3,4),
+                        Square(2,3,4,4)
+                    ],
+                    [
+                        Square(4,4,1,3),
+                        Square(1,4,2,3),
+                        Square(None,4,3,4),
+                        Square(None,4,4,4)
+                    ]
+                ]
+
+        file_reader = FileReader() 
+
+        actual = file_reader.convert_file_lines_to_puzzle(lines_of_file)
+
+        for i in range(4):
+            for j in range(4):
+                self.assertEqual(expected[i][j].number, actual[i][j].number)
+                self.assertEqual(expected[i][j].row, actual[i][j].row)
+                self.assertEqual(expected[i][j].column, actual[i][j].column)
+                self.assertEqual(expected[i][j].block, actual[i][j].block)
+
 if __name__ == '__main__':
     unittest.main()
