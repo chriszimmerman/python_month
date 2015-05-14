8df07304867077269849d7e44a3cbce437444316
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-14 19:34:43 -0400

    Solver can now detect invalid puzzles when the same number is in the same block multiple times.

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -2,14 +2,18 @@ class Solver():
     def is_valid(self, puzzle):
         rows = []
         columns = []
+        blocks = []
+
         for _ in puzzle:
             rows.append([])
             columns.append([])
+            blocks.append([])
 
         for row in puzzle:
             for square in row:
                 rows[square.row - 1].append(square)
-                columns[square.column- 1].append(square)
+                columns[square.column - 1].append(square)
+                blocks[square.block - 1].append(square)
 
         for row in rows:
             values_from_squares = map(lambda x: x.number, row)
@@ -23,6 +27,12 @@ class Solver():
             if self.__duplicate_numbers_in_row(numbers):
                 return False
            
+        for block in blocks:
+            values_from_squares = map(lambda x: x.number, block)
+            numbers = list(filter(lambda x: x != None, values_from_squares))
+            if self.__duplicate_numbers_in_row(numbers):
+                return False
+
         return True 
     
 
--- sudoku/test/solver_test.py
+++ sudoku/test/solver_test.py
@@ -50,16 +50,16 @@ class SudokuPuzzleSolverTest(unittest.TestCase):
                         Square(2,2,4,2)
                     ],
                     [
-                        Square(3,2,1,3),
-                        Square(3,2,2,3),
-                        Square(3,2,3,4),
-                        Square(3,2,4,4)
+                        Square(3,3,1,3),
+                        Square(3,3,2,3),
+                        Square(3,3,3,4),
+                        Square(3,3,4,4)
                     ],
                     [
-                        Square(4,2,1,3),
-                        Square(4,2,2,3),
-                        Square(4,2,3,4),
-                        Square(4,2,4,4)
+                        Square(4,4,1,3),
+                        Square(4,4,2,3),
+                        Square(4,4,3,4),
+                        Square(4,4,4,4)
                     ]
                 ]
         solver = Solver()
@@ -80,19 +80,50 @@ class SudokuPuzzleSolverTest(unittest.TestCase):
                         Square(4,2,4,2)
                     ],
                     [
-                        Square(1,2,1,3),
-                        Square(2,2,2,3),
-                        Square(3,2,3,4),
-                        Square(4,2,4,4)
+                        Square(1,3,1,3),
+                        Square(2,3,2,3),
+                        Square(3,3,3,4),
+                        Square(4,3,4,4)
                     ],
                     [
-                        Square(1,2,1,3),
-                        Square(2,2,2,3),
-                        Square(3,2,3,4),
-                        Square(4,2,4,4)
+                        Square(1,4,1,3),
+                        Square(2,4,2,3),
+                        Square(3,4,3,4),
+                        Square(4,4,4,4)
                     ]
                 ]
         solver = Solver()
         self.assertFalse(solver.is_valid(puzzle))
+
+    def test_is_valid_return_false_when_two_of_same_number_in_block(self):
+        puzzle = [
+                    [
+                        Square(1,1,1,1),
+                        Square(None,1,2,1),
+                        Square(None,1,3,2),
+                        Square(None,1,4,2)
+                    ],
+                    [
+                        Square(None,2,1,1),
+                        Square(1,2,2,1),
+                        Square(None,2,3,2),
+                        Square(None,2,4,2)
+                    ],
+                    [
+                        Square(None,3,1,3),
+                        Square(None,3,2,3),
+                        Square(None,3,3,4),
+                        Square(None,3,4,4)
+                    ],
+                    [
+                        Square(None,4,1,3),
+                        Square(None,4,2,3),
+                        Square(None,4,3,4),
+                        Square(None,4,4,4)
+                    ]
+                ]
+        solver = Solver()
+        self.assertFalse(solver.is_valid(puzzle))
+
 if __name__ == '__main__':
     unittest.main()
