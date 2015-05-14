bd9ba59801101f1e692dd71d552ca155381608a3
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-14 19:25:10 -0400

    Sudoku solver can now detecct invalid puzzles when the same number is in the same column multiple times

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,10 +1,28 @@
 class Solver():
     def is_valid(self, puzzle):
+        rows = []
+        columns = []
+        for _ in puzzle:
+            rows.append([])
+            columns.append([])
+
         for row in puzzle:
+            for square in row:
+                rows[square.row - 1].append(square)
+                columns[square.column- 1].append(square)
+
+        for row in rows:
             values_from_squares = map(lambda x: x.number, row)
             numbers = list(filter(lambda x: x != None, values_from_squares))
             if self.__duplicate_numbers_in_row(numbers):
                 return False
+
+        for column in columns:
+            values_from_squares = map(lambda x: x.number, column)
+            numbers = list(filter(lambda x: x != None, values_from_squares))
+            if self.__duplicate_numbers_in_row(numbers):
+                return False
+           
         return True 
     
 
--- sudoku/test/solver_test.py
+++ sudoku/test/solver_test.py
@@ -20,16 +20,16 @@ class SudokuPuzzleSolverTest(unittest.TestCase):
                         Square(1,2,4,2)
                     ],
                     [
-                        Square(4,2,1,3),
-                        Square(1,2,2,3),
-                        Square(2,2,3,4),
-                        Square(3,2,4,4)
+                        Square(4,3,1,3),
+                        Square(1,3,2,3),
+                        Square(2,3,3,4),
+                        Square(3,3,4,4)
                     ],
                     [
-                        Square(2,2,1,3),
-                        Square(3,2,2,3),
-                        Square(1,2,3,4),
-                        Square(4,2,4,4)
+                        Square(2,4,1,3),
+                        Square(3,4,2,3),
+                        Square(1,4,3,4),
+                        Square(4,4,4,4)
                     ]
                 ]
         solver = Solver()
@@ -38,32 +38,61 @@ class SudokuPuzzleSolverTest(unittest.TestCase):
     def test_is_valid_return_false_when_two_of_same_number_in_row(self):
         puzzle = [
                     [
-                        Square(2,1,1,1),
-                        Square(4,1,2,1),
-                        Square(3,1,3,2),
-                        Square(2,1,4,2)
+                        Square(1,1,1,1),
+                        Square(1,1,2,1),
+                        Square(1,1,3,2),
+                        Square(1,1,4,2)
                     ],
                     [
-                        Square(3,2,1,1),
+                        Square(2,2,1,1),
                         Square(2,2,2,1),
-                        Square(4,2,3,2),
-                        Square(1,2,4,2)
+                        Square(2,2,3,2),
+                        Square(2,2,4,2)
                     ],
                     [
-                        Square(4,2,1,3),
-                        Square(1,2,2,3),
-                        Square(2,2,3,4),
+                        Square(3,2,1,3),
+                        Square(3,2,2,3),
+                        Square(3,2,3,4),
                         Square(3,2,4,4)
                     ],
                     [
-                        Square(2,2,1,3),
-                        Square(3,2,2,3),
-                        Square(1,2,3,4),
+                        Square(4,2,1,3),
+                        Square(4,2,2,3),
+                        Square(4,2,3,4),
                         Square(4,2,4,4)
                     ]
                 ]
         solver = Solver()
         self.assertFalse(solver.is_valid(puzzle))
         
+    def test_is_valid_return_false_when_two_of_same_number_in_column(self):
+        puzzle = [
+                    [
+                        Square(1,1,1,1),
+                        Square(2,1,2,1),
+                        Square(3,1,3,2),
+                        Square(4,1,4,2)
+                    ],
+                    [
+                        Square(1,2,1,1),
+                        Square(2,2,2,1),
+                        Square(3,2,3,2),
+                        Square(4,2,4,2)
+                    ],
+                    [
+                        Square(1,2,1,3),
+                        Square(2,2,2,3),
+                        Square(3,2,3,4),
+                        Square(4,2,4,4)
+                    ],
+                    [
+                        Square(1,2,1,3),
+                        Square(2,2,2,3),
+                        Square(3,2,3,4),
+                        Square(4,2,4,4)
+                    ]
+                ]
+        solver = Solver()
+        self.assertFalse(solver.is_valid(puzzle))
 if __name__ == '__main__':
     unittest.main()
