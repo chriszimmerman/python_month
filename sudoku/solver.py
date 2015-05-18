7cc97e29db806f618f3b09eeca490f755f746129
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-18 19:37:40 -0400

    Implemented brute force sudoku solver

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -37,4 +37,45 @@ class Solver():
         return self.__check_for_uniqueness(puzzle, lambda x: x == x)
 
     def solve(self, puzzle):
-       return [[]] 
+        length = len(puzzle)
+        already_solved_squares = []
+        potentially_solved_squares = []
+        squares_to_solve = []
+        backtrack_stack = []
+    
+        for row in puzzle:
+            for square in row:
+                if square.number == None:
+                    squares_to_solve.append(square)
+                else:
+                    already_solved_squares.append(square)
+
+        take_from_potential_solution = False
+        square_tried = False
+       
+        while len(squares_to_solve) > 0 or len(backtrack_stack) > 0:
+            if take_from_potential_solution and len(potentially_solved_squares) > 0:
+                current_square = potentially_solved_squares.pop()
+            elif len(backtrack_stack) > 0: 
+                current_square = backtrack_stack.pop()
+            else:
+                current_square = squares_to_solve.pop()
+
+            if current_square.number == None:
+                current_square.number = 0
+
+            for _ in range(length):
+                current_square.number = current_square.number % length + 1
+            
+                if self.is_valid(puzzle): 
+                    potentially_solved_squares.append(current_square)
+                    take_from_potential_solution = False
+                    square_tried = True
+                    break
+               
+            if not square_tried:
+                backtrack_stack.append(current_square)
+                take_from_potential_solution = True 
+                square_tried = False
+
+        return puzzle 
