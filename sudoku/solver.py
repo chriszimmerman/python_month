16d674db6a5496dd4876a13654dbab7ada03f5f3
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-17 21:28:24 -0400

    Added failing test for solve() method. Added check for unique elements in a given collection without filtering out blank squares. This will help determine is the puzzle has been solved.

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -19,9 +19,6 @@ class Solver():
             and self.__contains_unique_squares(columns)
             and self.__contains_unique_squares(blocks))
 
-    def __duplicate_numbers_in_row(self, numbers):
-        return len(numbers) != len(set(numbers))
-
     def __contains_unique_squares(self, collections):
         for collection in collections:
             values_from_squares = map(lambda x: x.number, collection)
@@ -29,3 +26,36 @@ class Solver():
             if self.__duplicate_numbers_in_row(numbers):
                 return False
         return True
+
+    def __duplicate_numbers_in_row(self, numbers):
+        return len(numbers) != len(set(numbers))
+
+    def is_complete(self, puzzle):
+        rows = []
+        columns = []
+        blocks = []
+
+        for _ in puzzle:
+            rows.append([])
+            columns.append([])
+            blocks.append([])
+
+        for row in puzzle:
+            for square in row:
+                rows[square.row - 1].append(square)
+                columns[square.column - 1].append(square)
+                blocks[square.block - 1].append(square)
+
+        return ( self.__contains_unique_squares_not_filtering_blanks(rows)
+            and self.__contains_unique_squares_not_filtering_blanks(columns)
+            and self.__contains_unique_squares_not_filtering_blanks(blocks))
+
+    def __contains_unique_squares_not_filtering_blanks(self, collections):
+        for collection in collections:
+            numbers = list(map(lambda x: x.number, collection))
+            if self.__duplicate_numbers_in_row(numbers):
+                return False
+        return True
+
+    def solve(self, puzzle):
+       return [[]] 
--- sudoku/test/solver_test.py
+++ sudoku/test/solver_test.py
@@ -125,5 +125,132 @@ class SudokuPuzzleSolverTest(unittest.TestCase):
         solver = Solver()
         self.assertFalse(solver.is_valid(puzzle))
 
+    def test_is_complete(self):
+        puzzle = [
+                    [
+                        Square(1,1,1,1),
+                        Square(4,1,2,1),
+                        Square(2,1,3,2),
+                        Square(3,1,4,2)
+                    ],
+                    [
+                        Square(2,2,1,1),
+                        Square(3,2,2,1),
+                        Square(4,2,3,2),
+                        Square(1,2,4,2)
+                    ],
+                    [
+                        Square(4,3,1,3),
+                        Square(1,3,2,3),
+                        Square(3,3,3,4),
+                        Square(2,3,4,4)
+                    ],
+                    [
+                        Square(3,4,1,3),
+                        Square(2,4,2,3),
+                        Square(1,4,3,4),
+                        Square(4,4,4,4)
+                    ]
+                ]
+
+        solver = Solver()
+        self.assertTrue(solver.is_complete(puzzle))
+
+    def test_is_complete_returns_false_with_blanks(self):
+        puzzle = [
+                    [
+                        Square(None,1,1,1),
+                        Square(4,1,2,1),
+                        Square(2,1,3,2),
+                        Square(3,1,4,2)
+                    ],
+                    [
+                        Square(2,2,1,1),
+                        Square(None,2,2,1),
+                        Square(None,2,3,2),
+                        Square(1,2,4,2)
+                    ],
+                    [
+                        Square(None,3,1,3),
+                        Square(None,3,2,3),
+                        Square(None,3,3,4),
+                        Square(None,3,4,4)
+                    ],
+                    [
+                        Square(3,4,1,3),
+                        Square(2,4,2,3),
+                        Square(None,4,3,4),
+                        Square(None,4,4,4)
+                    ]
+                ]
+
+        solver = Solver()
+        self.assertFalse(solver.is_complete(puzzle))
+
+    def test_solve(self):
+        puzzle = [
+                    [
+                        Square(1,1,1,1),
+                        Square(None,1,2,1),
+                        Square(2,1,3,2),
+                        Square(None,1,4,2)
+                    ],
+                    [
+                        Square(None,2,1,1),
+                        Square(None,2,2,1),
+                        Square(4,2,3,2),
+                        Square(None,2,4,2)
+                    ],
+                    [
+                        Square(None,3,1,3),
+                        Square(1,3,2,3),
+                        Square(3,3,3,4),
+                        Square(None,3,4,4)
+                    ],
+                    [
+                        Square(3,4,1,3),
+                        Square(None,4,2,3),
+                        Square(None,4,3,4),
+                        Square(4,4,4,4)
+                    ]
+                ]
+
+        expected = [
+                    [
+                        Square(1,1,1,1),
+                        Square(4,1,2,1),
+                        Square(2,1,3,2),
+                        Square(3,1,4,2)
+                    ],
+                    [
+                        Square(2,2,1,1),
+                        Square(3,2,2,1),
+                        Square(4,2,3,2),
+                        Square(1,2,4,2)
+                    ],
+                    [
+                        Square(4,3,1,3),
+                        Square(1,3,2,3),
+                        Square(3,3,3,4),
+                        Square(2,3,4,4)
+                    ],
+                    [
+                        Square(3,4,1,3),
+                        Square(2,4,2,3),
+                        Square(1,4,3,4),
+                        Square(4,4,4,4)
+                    ]
+                ]
+
+        solver = Solver()
+        actual = solver.solve(puzzle)
+
+        for row in range(len(expected)):
+            for column in range(len(expected)):
+                self.assertEqual(expected[row][column].number, actual[row][column].number)
+                self.assertEqual(expected[row][column].row, actual[row][column].row)
+                self.assertEqual(expected[row][column].column, actual[row][column].column)
+                self.assertEqual(expected[row][column].block, actual[row][column].block)
+
 if __name__ == '__main__':
     unittest.main()
