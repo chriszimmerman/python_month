c05e384ee398ace19cd888315dde206cfccb2c30
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-05-22 20:38:40 -0400

    added bijective functions in URLShortener to encode and decode a URL based on a row id from a database

--- url/test/url_shortener_test.py
+++ url/test/url_shortener_test.py
@@ -1,7 +1,43 @@
+import unittest
+from url_shortener import URLShortener
+
 class URLShortenerTest(unittest.TestCase):
 
-    def test_truth(self):
-        self.assertTrue(True)
+    def test_encode_url(self):
+        sut = URLShortener()
+
+        rowId = 125
+        expected = "cb" 
+        actual = sut.encode_url(rowId)
+
+        self.assertEqual(expected, actual)
+
+    def test_encode_url2(self):
+        sut = URLShortener()
+
+        rowId = 19158 
+        expected = "e9a" 
+        actual = sut.encode_url(rowId)
+
+        self.assertEqual(expected, actual)
+
+    def test_decode_url(self):
+        sut = URLShortener()
+
+        url = "cb" 
+        expected = 125
+        actual = sut.decode_url(url)
+
+        self.assertEqual(expected, actual)
+
+    def test_decode_url2(self):
+        sut = URLShortener()
+
+        url = "e9a" 
+        expected = 19158 
+        actual = sut.decode_url(url)
+
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
--- url/url_shortener.py
+++ url/url_shortener.py
@@ -10,12 +10,53 @@ conn.commit()
 conn.close()
 
 class URLShortener:
-    def shorten_url(self, url):
-        return 0; 
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    alphabet_length = len(alphabet)
 
-#create database
+    def encode_url(self, rowId):
+        reverse_alphabet_digits = self.__get_alphabet_digits_from_rowid(rowId)[::-1]
+        url_characters = map(self.__get_url_character, reverse_alphabet_digits)
+
+        return ''.join(url_characters)
+
+    def __get_alphabet_digits_from_rowid(self, rowId):
+        alphabet_digits = []
+
+        while rowId > 0:
+            digit = rowId % self.alphabet_length
+            alphabet_digits.append(digit)
+            rowId = rowId // self.alphabet_length
+
+        return alphabet_digits
+
+    def __get_url_character(self, digit):
+        return self.alphabet[digit]
+
+    def decode_url(self, url):
+        alphabet_digits = self.__get_alphabet_digits_from_url(url)
+        digits_by_power = self.__convert_alphabet_digits_to_decimal(alphabet_digits) 
+        return sum(digits_by_power) 
+
+    def __get_alphabet_digits_from_url(self, url):
+        return list(map(lambda char: self.alphabet.index(char), url))
+
+    def __convert_alphabet_digits_to_decimal(self, digits):
+        reverse_digits = digits[::-1]
+        digits_by_power = [] 
+
+        for digit in reverse_digits:
+            digits_by_power.append(self.__to_decimal(digit, reverse_digits))
+
+        return digits_by_power 
+
+    def __to_decimal(self, digit, digits):
+        return digit * self.alphabet_length ** digits.index(digit)
+
+        
+
+#create database - DONE
 #take url and save in db
-#get row id and convert id to string
+#get row id and convert id to string - DONE
 #this string is the url shared
-#when string is used as input, perform inverse function from string to row id
+#when string is used as input, perform inverse function from string to row id - DONE
 #get that row from the database
